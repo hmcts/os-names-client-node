f62d0761464902edc51ddd0dca8af328c1c7998c
andrewwa-kainos <andrewwa@kainos.com>
2019-01-21 14:39:21 +0000

    Update and add tests

--- src/postcodeToCountryClient.ts
+++ src/postcodeToCountryClient.ts
@@ -2,11 +2,13 @@ import * as requestDefault from 'request'
 import * as requestPromise from 'request-promise-native'
 
 export class PostcodeToCountryClient {
-    constructor (private readonly apiToken: string,
-                 private readonly request: requestDefault.RequestAPI<requestPromise.RequestPromise,
-                     requestPromise.RequestPromiseOptions,
-                     requestDefault.RequiredUriUrl> = requestPromise,
-                 private readonly apiUrl: string = 'https://api.ordnancesurvey.co.uk') {
+    constructor (
+        private readonly apiToken: string,
+        private readonly request: requestDefault.RequestAPI<requestPromise.RequestPromise,
+            requestPromise.RequestPromiseOptions,
+            requestDefault.RequiredUriUrl> = requestPromise,
+        private readonly apiUrl: string = 'https://api.ordnancesurvey.co.uk'
+    ) {
     }
 
     public async lookupCountry (postcode: string): Promise<string> {
@@ -16,12 +18,12 @@ export class PostcodeToCountryClient {
 
         const uri: string = this.apiUrl + `/opennames/v1/find?query=${postcode}&maxresults=1&key=${this.apiToken}`
         return this.request.get({
-            json: false,
+            json: true,
             resolveWithFullResponse: true,
             simple: false,
             uri: uri
-        }).then((response) => {
-            return response.body.results[0]['GAZETTEER_ENTRY']['COUNTRY']
         })
+            .then((response) => response.body.results[0]['GAZETTEER_ENTRY']['COUNTRY'])
+            .catch(reason => Promise.reject(`Unable to find country for '${postcode}': ${reason}`))
     }
 }
